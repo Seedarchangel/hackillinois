5fe952beb009c831fa3f53b8f3e6d10bf76b0e2d
lingkai kong <lingkai2@illinois.edu>
2018-02-24 16:35:25 -0600

    user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,107 @@
-var User = require('../models/user')
+var User = require('../models/user');
+var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+var MongoClient = require('mongodb').MongoClient
+var mongodburl = "mongodb://<tester>:<testerone>@ds247698.mlab.com:47698/hackillinois"
 
 exports.user_register = function(req, res, next) {
-    res.send("NOT IMPLEMENTED: user register");
+    var username = req.post["username"];
+    var email = req.post["email"];
+    var github_name = req.post["github_name"];
+    var password = req.post["password"];
+    var confirmpassword = req.post["confirmpassword"];
+    var response;
+    if(username === ""){
+      response = JSON.parse({"status":"error","message":"username cannot be empty"})
+      res.send(response);
+    }
+    if(email === ""){
+      response = JSON.parse({"status":"error","message":"email cannot be empty"})
+      res.send(response);
+    }
+    if(password === ""){
+      response = JSON.parse({"status":"error","message":"password cannot be empty"})
+      res.send(response);
+    }
+    if(confirmpassword === ""){
+      response = JSON.parse({"status":"error","message":"confirmpassword cannot be empty"})
+      res.send(response);
+    }
+    if(password === confirmpassword){
+      response = JSON.parse({"status":"error","message":"password and confirmed password do not match"})
+      res.send(response);
+    }
+    MongoClient.connect(mongodbUrl, function (err, db) {
+    var collection = db.collection('users');
+    collection.findOne({'username' : username})
+      .then(function (result) {
+        if (null != result) {
+          response = JSON.parse({"status":"error","message":"user already exist"})
+          res.send(response);
+        }
+        else  {
+          var user = {
+            "username": username,
+            "password": password,
+            "email" : email,
+            "project" : null,
+            "current_task" : null,
+            "avatar" : "",
+            "github" : github_name
+          }
+
+          console.log("CREATING USER:", username);
+
+          collection.insert(user)
+            .then(function () {
+              db.close();
+              response = JSON.parse({"status":"ok","message":""})
+              res.send(response);
+            });
+        }
+      });
+  });
+
 };
 
 exports.user_login = function (req, res, next) {
-    res.send("NOT IMPLEMENTED: user login");
+  var curr_username = req.post["username"];
+  var curr_password = req.post["password"];
+  MongoClient.connect(mongodbUrl, function (err, db) {
+  var collection = db.collection('localUsers');
+
+  //check if username is already assigned in our database
+  collection.findOne({'username' : curr_username})
+    .then(function (result) {
+      if (null != result) {
+        if(result.body.password !== curr_password){
+          var response = JSON.parse({"status":"error","message":"user name and password does not match"})
+          res.send(response);
+        }
+        else{
+          var response = JSON.parse({"status":"ok","message":""})
+          res.send(response);
+        }
+      }
+      else  {
+        var response = JSON.parse({"status":"error","message":"user does not exist"})
+        res.send(response);
+      }
+    })
+  });
 };
 
 exports.user_get = function (req, res, next) {
-    res.send("NOT IMPLEMENTED: get user info");
+    var curr_username = req.post["username"];
+    MongoClient.connect(mongodbUrl, function (err, db) {
+    var collection = db.collection('localUsers');
+
+    //check if username is already assigned in our database
+    collection.findOne({'username' : curr_username})
+      .then(function (result) {
+        if (null != result) {
+            res.send(result)
+        }
+      })
+    })
 };
--- models/user.js
+++ models/user.js
@@ -7,8 +7,8 @@ var UserSchema = new Schema(
         username: { type: String, required: true, max: 100 },
         password: { type: String, required: true, max: 100 },
         email: { type: String, required: true, max: 100 },
-        project: {type: Schema.objectId, ref: 'Project'},
-        current_task: {type: Schema.obejctId, ref: 'Task'},
+        project: {type: Schema.ObjectId, ref: 'Project'},
+        current_task: {type: Schema.ObjectId, ref: 'Task'},
         avatar: {type: String},
         github: { type: String, max: 100 }
     }
