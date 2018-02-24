a5952ec99100aceb959014e6064219970bde51c5
Mirari <zf4@illinois.edu>
2018-02-24 16:39:30 -0600

    Project controller.

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,10 +1,37 @@
 var Project = require('../models/project')
 
-exports.project_get = function(req, res, next) {
-    //TODO: some test 
-    res.send("NOT IMPLEMENTED: project get");
+exports.project_get = function (req, res, next) {
+    Project.findOne({
+        name: req.body.project_name
+    }).lean().exec(function (err, result) {
+        if (err) {
+            return next(err);
+        }
+        if (result === null) {
+            var err = new Error('Project not found.');
+            err.status = 404;
+            return next(err);
+        }
+        res.send(result);
+    })
 };
 
 exports.project_post = function (req, res, next) {
-    res.send("NOT IMPLEMENTED: project post");
+    var project = new Project({
+        name: req.body.name,
+        description: req.body.project_description,
+        repo: req.body.repo_name
+    });
+    project.save(function (err) {
+        if (err) {
+            res.json({
+                status: 'error',
+                message: 'Create project error.'
+            });
+        } else
+            res.json({
+                status: 'ok',
+                message: 'Creation successful.'
+            });
+    })
 };
--- models/project.js
+++ models/project.js
@@ -7,10 +7,10 @@ var ProjectSchema = new Schema(
         name: { type: String, required: true, max: 100},
         description: { type: String, max: 200},
         repo: { type: String, max: 200},
-        total_task: {type: Number},
-        task_finished: { type: Number },
-        task_in_process: { type: Number },
-        task_unassigned: { type: Number },
+        total_task: {type: Number, default: 0},
+        task_finished: { type: Number, default: 0 },
+        task_in_process: { type: Number, default: 0 },
+        task_unassigned: { type: Number, default: 0 },
         task: [{ type: Schema.ObjectId, ref: 'Task'}]
     }
 );
