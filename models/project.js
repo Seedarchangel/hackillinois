bc0a4baceb9204d20fa9c9c974ef62ad39a1a194
Mirari <zf4@illinois.edu>
2018-02-24 19:01:31 -0600

    Auto format

--- models/project.js
+++ models/project.js
@@ -2,17 +2,40 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
-var ProjectSchema = new Schema(
-    {
-        name: { type: String, required: true, max: 100},
-        description: { type: String, max: 200},
-        repo: { type: String, max: 200},
-        total_task: {type: Number, default: 0},
-        task_finished: { type: Number, default: 0 },
-        task_in_progress: { type: Number, default: 0 },
-        task_unassigned: { type: Number, default: 0 },
-        task: [{ type: Schema.ObjectId, ref: 'Task'}]
-    }
-);
+var ProjectSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        max: 100
+    },
+    description: {
+        type: String,
+        max: 200
+    },
+    repo: {
+        type: String,
+        max: 200
+    },
+    total_task: {
+        type: Number,
+        default: 0
+    },
+    task_finished: {
+        type: Number,
+        default: 0
+    },
+    task_in_progress: {
+        type: Number,
+        default: 0
+    },
+    task_unassigned: {
+        type: Number,
+        default: 0
+    },
+    task: [{
+        type: Schema.ObjectId,
+        ref: 'Task'
+    }]
+});
 
 module.exports = mongoose.model('Project', ProjectSchema);
--- models/user.js
+++ models/user.js
@@ -2,16 +2,37 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
-var UserSchema = new Schema(
-    {
-        username: { type: String, required: true, max: 100 },
-        password: { type: String, required: true, max: 100 },
-        email: { type: String, required: true, max: 100 },
-        project: [{type: Schema.ObjectId, ref: 'Project'}],
-        current_task: {type: Schema.ObjectId, ref: 'Task'},
-        avatar: {type: String},
-        github: { type: String, max: 100 }
+var UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        max: 100
+    },
+    password: {
+        type: String,
+        required: true,
+        max: 100
+    },
+    email: {
+        type: String,
+        required: true,
+        max: 100
+    },
+    project: [{
+        type: Schema.ObjectId,
+        ref: 'Project'
+    }],
+    current_task: {
+        type: Schema.ObjectId,
+        ref: 'Task'
+    },
+    avatar: {
+        type: String
+    },
+    github: {
+        type: String,
+        max: 100
     }
-);
+});
 
 module.exports = mongoose.model('User', UserSchema);
